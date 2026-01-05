dae42389f15a233f54f6eab3d05bd2b8eec248a3
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-05 14:41:12 -0800

    Create for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,13 @@
+# Name: Abigail Bui
+# Period: 7
+# Assignment: For Homework
+# Time Spent: 5 min?
+
+x_values = [0, 1, 2, 3, 4, 5, 6] #numbers for each loop
+
+print ("y = 2x + 2") # the equation the for loop is solving
+
+# for loop
+
+for num in x_values:
+    print(f"if x = {num} then y = {(num*2)+2}") # solves the equation above for each value in the x-values list and prints the corresponding solutions for each value
