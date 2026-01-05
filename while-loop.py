30c4a8c167476812f090c82459aefcd103f65ff2
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-05 14:41:55 -0800

    Create while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,10 @@
+# Name: Abigail Bui
+# Period: 7
+# Assignment:  While Homework
+# Time Spent: 2 min?
+
+yes_no = "yes"
+while yes_no == "yes":
+	yes_no = input("Yes or no? ")
+	# if you answer the question with "yes" then it will ask the question again
+	# it will continue this loop until you answer the question with an answer that is not "yes"
