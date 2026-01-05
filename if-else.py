fae8f35ea2d50367feb14c37f81236910a7380b9
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-05 14:38:24 -0800

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,17 @@
+# Name: Abigail Bui
+# Period: 7
+# Assignment: If-Else Practice
+# Time Spent:  10 - 15 minutes
+
+age = int(input('How old are you?')) # asks age of user
+if age >= 18:
+  print ('You are a legal adult!') # if age is 18 or over code informs user they are a legal adult
+else:
+  print ('You are not yet a legal adult!') # if age is less than 18 code informs user they are not yet a legal adult
+
+name = str(input('What is your name? Type your answer in ALL CAPS:')) # asks name of user
+if name == 'ABIGAIL':
+  print ("Cool, that's the coder's name too!") # if user responds 'ABIGAIL' then code informs user the coder's name is also ABIGAIL
+else:
+  print ("That's a cool name!") # if user responds with another name code complements their name
+  
