fe4546d05fdf0f0285373e514b9aad3d82592bb8
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-07 15:05:15 -0800

    reading

--- With_HW.py
+++ With_HW.py
@@ -12,3 +12,15 @@ for num in range(1, entries + 1):
   entry = input('#{num} What would you like to add to the file?')
   step-file.write(f'{entry}\n')
 step-file.close()
+
+
+step-file = open('step_count_tracker.txt', 'r')
+line = step-list.readline()
+sum = 0
+
+while line = !'' :
+  sum += int(line)
+  line = step-file.readline()
+  
+step-file.close()
+print(f'There are {sum} total entries')
