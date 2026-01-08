f0e26b73a46693d5c8a5f6953f44fac476b16761
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-08 22:53:52 +0000

    completed the lecture yippe

--- With_HW.py
+++ With_HW.py
@@ -1,22 +1,20 @@
 # Name: Abigail Bui
 # Period: 7
 # Assignment: Week 1 HW - With
-# Time Spent: 
+# Time Spent: 2 class periods
+
 
 
 entries = int(input("How many entries are being entered?"))
 
-with open('step_count_tracker.txt', 'a') as stepfile:
+with open('step_count_tracker.txt', 'a') as file_var, open('step_count_tracker.txt', 'r') as file_rd:
   for num in range(1, entries + 1):
     entry = input(f'#{num} What would you like to add to the file?')
-    stepfile.write(f'{entry}\n')
-
-
-with open('step_count_tracker.txt', 'r') as stepfile:
-  line = stepfile.readline()
+    file_var.write(f'{entry}\n')
+  line = file_rd.readline()
   sum = 0
   while line != "" :
     sum += int(line)
-    line = stepfile.readline()
+    line = file_rd.readline()    
 
 print(f"You've competed a total of {sum} steps so far!")
