acaef9ae3eb15e873d6e87ea62059eaaf8ba5014
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-16 00:26:10 +0000

    yayyayayayayay

--- calculator.py
+++ calculator.py
@@ -6,6 +6,8 @@
 num_1 = int(input("Enter a number: "))
 num_2 = int(input("Enter a number: "))
 operation = input("Do you want to add(+), subtract(-), multiply(*) or divide(/)? ")
+result = ""
+
 
 try:
     if operation == "add" or operation == "Add" or operation == "ADD" or operation == "+" or operation == "(+)":
@@ -22,12 +24,19 @@ try:
         print(result)
     else:
         print("Please pick one of the four options presented.")
-except ZeroDivisionError:
-    print("You cannot divide a number by zero. Your answer is undefined.")
+        operation = input("Do you want to add(+), subtract(-), multiply(*) or divide(/)? ")
 except ValueError:
-    print("Please enter numbers.")
+    print("Please enter numbers. Try again.")
+except ZeroDivisionError:
+    print("You cannot divide a number by zero. Your answer is undefined. Try again.")
 except:
-    print("An error has occurred.")
-finally:
-    with open("math_record.txt", "a") as math_record:
-        
+    print("An error has occurred. Try again.")
+else:
+    with open("math_record.txt", "w") as math_record:
+        math_record.write(f'{operation}\n')
+        math_record.write(f'{num_1}\n')
+        math_record.write(f'{num_2}\n')
+        math_record.write(f'{result}\n')
+        print()
+        print('Your calculations have been recorded in math_record.txt')
+
