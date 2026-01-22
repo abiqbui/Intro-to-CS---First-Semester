544fbfcdff8be1e33493bafba971e829e6926ba9
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-22 23:01:13 +0000

    commit

--- edit-list.py
+++ edit-list.py
@@ -3,4 +3,18 @@
 # Assignment: Creating and Editing Lists HW
 # Time Spent: 
 
-num_of_val = input('How many values do you want to input into your list?')
+num_of_val = int(input('How many values do you want to input into your list?'))
+my_list = []
+num = 0 
+
+while num_of_val > 0:
+    num += 1
+    val = input(f"Value {num}: ")
+    my_list.append(val)
+    num_of_val -= 1
+
+print(my_list)
+
+edit_list = input('Would you like to edit the list?')
+if edit_list = 'yes' or edit_list = 'Yes' or edit_list = 'YES':
+    
