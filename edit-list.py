586e5a98bb54fec8ecd11faeac8b0b79d479778e
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-23 20:19:10 +0000

    done perhaps?

--- edit-list.py
+++ edit-list.py
@@ -3,7 +3,7 @@
 # Assignment: Creating and Editing Lists HW
 # Time Spent: 
 
-num_of_val = int(input('How many values do you want to input into your list?'))
+num_of_val = int(input('How many values do you want to input into your list? '))
 my_list = []
 num = 0 
 
@@ -15,6 +15,12 @@ while num_of_val > 0:
 
 print(my_list)
 
-edit_list = input('Would you like to edit the list?')
-if edit_list = 'yes' or edit_list = 'Yes' or edit_list = 'YES':
-    
+edit_list = input('Would you like to edit the list? (Yes or No) ')
+
+while edit_list == 'yes' or edit_list == 'Yes' or edit_list == 'YES':
+    value_num = int(input('What value do you want to edit? (1, 2, 3, 4, etc.) '))
+    change = input('What would you like to change this value to? ')
+    my_list[value_num-1] = change
+    print(my_list)
+    edit_list = input('Would you like to edit the list?')
+
