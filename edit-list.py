fee22cc0616db56b69c7151bef4795400357587f
abiqbui <13097@edu.wheatlandhigh.org>
2026-01-23 22:41:03 +0000

    comments

--- edit-list.py
+++ edit-list.py
@@ -7,39 +7,25 @@ num_of_val = int(input('How many values do you want to input into your list? '))
 my_list = [] # empty list to add values to
 num = 0 # start count of the value number to be referenced below
 
-while num_of_val > 0:
+while num_of_val > 0: 
     num += 1
-    val = input(f"Value {num}: ")
-    my_list.append(val)
-    num_of_val -= 1
+    val = input(f"Value {num}: ") # gets values
+    my_list.append(val) # adds your value input to the list
+    num_of_val -= 1 # subtracts 1 for num of val so the loop ends when you've inputed all your values
 
 print(my_list)
 
 def editing():
-    edit_list = input('Would you like to edit the list? (Yes or No) ')
+    edit_list = input('Would you like to edit the list? (Yes or No) ') # asks to edit list
     
-    while edit_list == 'yes' or edit_list == 'Yes' or edit_list == 'YES':
-        value_num = int(input('What value do you want to edit? (1, 2, 3, 4, etc.) '))
-        change = input('What would you like to change this value to? ')
-        my_list[value_num-1] = change
-        print(my_list)
-        edit_list = input('Would you like to edit the list? ')
+    while edit_list == 'yes' or edit_list == 'Yes' or edit_list == 'YES': # if user wants to edit list execute this loop
+        value_num = int(input('What value do you want to edit? (1, 2, 3, 4, etc.) ')) # get index number of what value to edit
+        change = input('What would you like to change this value to? ') # get the new value
+        my_list[value_num-1] = change # change old value to new value
+        print(my_list) # print list with new changes
+        edit_list = input('Would you like to edit the list? ') # start loop again if they want to edit more
 try:
-    editing()
+    editing() # calls the editing function
 except IndexError: 
     print("Try again. You don't have that number value (1, 2, 3, 4, etc.)")
-    editing()
-def editing():
-    edit_list = input('Would you like to edit the list? (Yes or No) ')
-    
-    while edit_list == 'yes' or edit_list == 'Yes' or edit_list == 'YES':
-        value_num = int(input('What value do you want to edit? (1, 2, 3, 4, etc.) '))
-        change = input('What would you like to change this value to? ')
-        my_list[value_num-1] = change
-        print(my_list)
-        edit_list = input('Would you like to edit the list?')
-try:
-    editing()
-except IndexError: 
-    print("Try again. You don't have that number value (1, 2, 3, 4, etc.)")
-    editing()
+    editing() # calls the editing function again so they can try again
