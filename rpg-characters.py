68ed2a1e5e74cf6fb7562f977e7d81fed78b2adb
abiqbui <13097@edu.wheatlandhigh.org>
2026-04-23 22:03:17 +0000

    fgjtuvy

--- rpg-characters.py
+++ rpg-characters.py
@@ -13,8 +13,25 @@ class RPG_Character:
         self.type = type
         self.damage_dealt = damage_dealt
         print(f"{self.name} uses {self.type}. Enemy takes {self.damage_dealt} damage.")
-    def take_damage(self, health, damage_taken)
+    def take_damage(self, damage_taken):
         self.health = self.max_health
+        self.damage_taken = damage_taken
+        new_health = self.health - self.damage_taken
+        print(f"Health is now at {new_health}.")
+    def display_stats(self):
+        print(f"Name: {self.name}  Max Health: {self.max_health}  Attack Power: {self.attack_power}  Level: {self.level}")
+
+class Healer (RPG_Character):
+    def __init__(self, name, max_health, attack_power, level, magic_type):
+        RPG_Character.__init__(self, name, max_health, attack_power, level)
+        self.magic_type = magic_type
+    def take_damage(self, damage_taken):
+        self.health = self.max_health
+        self.damage_taken = damage_taken/2
+        new_health = self.health - self.damage_taken
+        print(f"Health is now at {new_health}.")
+    def self_heal(self):
+        self.health = self.max_health
+        print(f"Health is now at {self.max_health}. Max health!")
+    def heal_ally(self, ally_health, ally_damage):
         
-        print(f"Health is now at {self.health}.")
-    def display_stats(self, )
