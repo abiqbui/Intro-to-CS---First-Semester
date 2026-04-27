864e30892b1b7493d687a85e2e26c49a5419390f
abiqbui <13097@edu.wheatlandhigh.org>
2026-04-27 04:42:24 +0000

    im tired

--- rpg-characters.py
+++ rpg-characters.py
@@ -1,7 +1,7 @@
 # Name: Abigail Bui
 # Period: 7
 # Assignment: Inheritence RPG Character
-# Time Spent: 
+# Time Spent: 2 hours?
 
 class RPG_Character:
     def __init__(self, name, max_health, attack_power, level):
@@ -33,5 +33,22 @@ class Healer (RPG_Character):
     def self_heal(self):
         self.health = self.max_health
         print(f"Health is now at {self.max_health}. Max health!")
-    def heal_ally(self, ally_health, ally_damage):
-        
+    def heal_ally(self, ally_name, ally_health, ally_damage):
+        self.ally_name = ally_name
+        self.ally_damage = ally_damage
+        self.ally_health = ally_health
+        heal_health = self.ally_health + (self.ally_damage/2)
+        print(f"{self.ally_name}'s health has regenerated to {heal_health}")
+
+class Support (RPG_Character):
+    def __init__(self, name, max_health, attack_power, level, magic_type):
+        RPG_Character.__init__(self, name, max_health, attack_power, level)
+        self.magic_type = magic_type
+    def display_stats(self, durability):
+        self.durability = durability
+        print(f"Name: {self.name}  Max Health: {self.max_health}  Attack Power: {self.attack_power}  Level: {self.level}  Durability: {self.durability}/10")
+    def shield(self):
+        damage_absorbed = self.damage_taken * (self.durability/10)
+        percentage = self.durability * 10
+        self.damage_taken = self.damage_taken - self.durability
+        print(f"Shield absorbs {percentage}% of the damage.")
